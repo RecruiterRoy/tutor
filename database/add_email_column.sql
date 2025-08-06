3cfaedeefb6d59bba1281f065cad77c5782a3513
Ritesh Roy <ritesh.manager@gmail.com>
2025-08-06 08:52:29 -0500

    Remove syllabus and other files from Git tracking - APK size fix

--- database/add_email_column.sql
+++
@@ -1,6 +0,0 @@
--- Add email column to user_profiles table
-ALTER TABLE user_profiles
-ADD COLUMN email VARCHAR;
-
--- Add a comment to document the email column
-COMMENT ON COLUMN user_profiles.email IS 'User email address, copied from auth.users table.'; 
--- database/add_school_name_column.sql
+++
@@ -1,6 +0,0 @@
--- Add school_name column to user_profiles table
-ALTER TABLE user_profiles 
-ADD COLUMN school_name VARCHAR(255);
-
--- Add a comment to document the school_name column
-COMMENT ON COLUMN user_profiles.school_name IS 'School name of the student'; 
